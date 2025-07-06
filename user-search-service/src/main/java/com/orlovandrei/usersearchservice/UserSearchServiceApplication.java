94693ef40b0b1627777b2b630fbe33dfd0ccfa3d
yattorie <pyryryqw882@gmail.com>
2025-07-06 16:30:54 +0300

    feat: add user-search-service

---
+++ user-search-service/src/main/java/com/orlovandrei/usersearchservice/UserSearchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.orlovandrei.usersearchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSearchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserSearchServiceApplication.class, args);
+	}
+
+}
---
+++ user-search-service/src/test/java/com/orlovandrei/usersearchservice/UserSearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orlovandrei.usersearchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSearchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
