43ac27620b921f813b070920c13354bbf65e1e6e
yattorie <pyryryqw882@gmail.com>
2025-08-06 01:09:18 +0300

    feat(order-service): add LoggerUtil for logging

---
+++ order-service/src/main/java/com/orlovandrei/orderservice/util/LoggerUtil.java
@@ -0,0 +1,12 @@
+package com.orlovandrei.orderservice.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggerUtil {
+    private static final Logger logger = LoggerFactory.getLogger(LoggerUtil.class);
+
+    public static void logError(String message, Throwable throwable) {
+        logger.error(message, throwable);
+    }
+}
