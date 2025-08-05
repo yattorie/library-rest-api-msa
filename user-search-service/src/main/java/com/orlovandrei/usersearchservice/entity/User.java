6bde95b6de2e3e6b882e0a322d576f06d3c9fb59
yattorie <pyryryqw882@gmail.com>
2025-08-06 01:12:48 +0300

    feat(user-search-service): add User entity and Role enum

---
+++ user-search-service/src/main/java/com/orlovandrei/usersearchservice/entity/User.java
@@ -0,0 +1,46 @@
+package com.orlovandrei.usersearchservice.entity;
+
+import com.orlovandrei.usersearchservice.entity.enums.Role;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    Long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    String username;
+
+    @Column(name = "email", nullable = false, unique = true)
+    String email;
+
+    @Column(name = "password", nullable = false)
+    String password;
+
+    @Enumerated(EnumType.STRING)
+    Role role;
+}
---
+++ user-search-service/src/main/java/com/orlovandrei/usersearchservice/entity/enums/Role.java
@@ -0,0 +1,6 @@
+package com.orlovandrei.usersearchservice.entity.enums;
+
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN
+}
