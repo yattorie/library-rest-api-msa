7d0297c6bb5686236b0671129b5fc3769ff73ebe
yattorie <pyryryqw882@gmail.com>
2025-08-05 15:46:49 +0300

    feat(auth-service): add a global exception handler Implemented: - @RestControllerAdvice with handlers for different types of exceptions - Custom exceptions (UserAlreadyExistsException and EmailAlreadyExistsException) - ExceptionBody to standardize error responses - Logging of all errors via LoggerUtil

---
+++ auth-service/src/main/java/com/orlovandrei/authservice/exception/EmailAlreadyExistsException.java
@@ -0,0 +1,11 @@
+package com.orlovandrei.authservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class EmailAlreadyExistsException extends RuntimeException {
+    public EmailAlreadyExistsException(String message) {
+        super(message);
+    }
+}
