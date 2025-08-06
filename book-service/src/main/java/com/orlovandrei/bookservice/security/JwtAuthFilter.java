ccd2ecdfa445cac0b0357200fba8f9d0ad03fd71
yattorie <pyryryqw882@gmail.com>
2025-08-06 03:25:26 +0300

    feat(book-service): implement JWT-based security configuration Added: - Security Config - JwtService for token operations - JwtAuthFilter for request processing - CustomUserDetailsService

---
+++ book-service/src/main/java/com/orlovandrei/bookservice/security/JwtAuthFilter.java
@@ -0,0 +1,61 @@
+package com.orlovandrei.bookservice.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        final String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = authHeader.substring(7);
+        String username = jwtService.extractUsernameFromToken(token);
+        String role = jwtService.extractAllClaims(token).get("role", String.class);
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+            List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(role));
+
+            UserDetails userDetails = new User(username, "", authorities);
+
+            if (jwtService.validateTokenForUser(token, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, authorities);
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
