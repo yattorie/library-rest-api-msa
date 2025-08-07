cb422a60327bee8b2e4f835c59883879e81e9dff
yattorie <pyryryqw882@gmail.com>
2025-08-08 00:09:56 +0300

    feat: enable Feign clients in order-service and user-search-service

--- user-search-service/src/main/java/com/orlovandrei/usersearchservice/UserSearchServiceApplication.java
+++ user-search-service/src/main/java/com/orlovandrei/usersearchservice/UserSearchServiceApplication.java
@@ -2,8 +2,10 @@ package com.orlovandrei.usersearchservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class UserSearchServiceApplication {
 
 	public static void main(String[] args) {
