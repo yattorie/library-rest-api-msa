a73c3b2c1d4488df6eb329d8e1b715d524c964f4
yattorie <pyryryqw882@gmail.com>
2025-08-08 15:01:34 +0300

    feat(user-search-service): add user DTO with documentation

---
+++ user-search-service/src/main/java/com/orlovandrei/usersearchservice/dto/UserDto.java
@@ -0,0 +1,29 @@
+package com.orlovandrei.usersearchservice.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Schema(description = "User data transfer object")
+public class UserDto {
+    @Schema(
+            description = "Unique identifier of the user",
+            example = "1"
+    )
+    Long id;
+
+    @Schema(
+            description = "Username of the user",
+            example = "test_user"
+    )
+    String username;
+
+    @Schema(
+            description = "Email address of the user",
+            example = "test_user@example.com"
+    )
+    String email;
+}
