0c933ed162cf47b7a1c885e05f891a42f082bdc0
yattorie <pyryryqw882@gmail.com>
2025-08-10 14:39:16 +0300

    feat(user-search-service): add new enum ACCESS_DENIED_INTERNAL_GET

--- user-search-service/src/main/java/com/orlovandrei/usersearchservice/util/Messages.java
+++ user-search-service/src/main/java/com/orlovandrei/usersearchservice/util/Messages.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 public enum Messages {
     USER_NOT_FOUND_ID("User not found with id: "),
     USER_NOT_FOUND_USERNAME("User not found with username: "),
+    ACCESS_DENIED_INTERNAL_GET("Only order-service can get user by username"),
     USER_NOT_FOUND_EMAIL("User not found with email: ");
     private final String message;
 }
