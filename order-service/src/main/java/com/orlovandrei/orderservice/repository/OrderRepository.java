4c19f8a8b2fe1da381c0ea5e9b1251081ef98df2
yattorie <pyryryqw882@gmail.com>
2025-08-10 19:31:44 +0300

    feat(order-service): add existsByUserIdAndBookIdAndStatusIn method

--- order-service/src/main/java/com/orlovandrei/orderservice/repository/OrderRepository.java
+++ order-service/src/main/java/com/orlovandrei/orderservice/repository/OrderRepository.java
@@ -1,10 +1,13 @@
 package com.orlovandrei.orderservice.repository;
 
 import com.orlovandrei.orderservice.entity.Order;
+import com.orlovandrei.orderservice.entity.enums.OrderStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
     Page<Order> findByUserId(Long userId, Pageable pageable);
-}
+    boolean existsByUserIdAndBookIdAndStatusIn(Long userId, Long bookId, List<OrderStatus> statuses);}
