9e700ac94d1a2ad9354cae03e67d38b37d46905b
yattorie <pyryryqw882@gmail.com>
2025-08-10 14:46:21 +0300

    feat(order-service): add OrderRepository

---
+++ order-service/src/main/java/com/orlovandrei/orderservice/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.orlovandrei.orderservice.repository;
+
+import com.orlovandrei.orderservice.entity.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    Page<Order> findByUserId(Long userId, Pageable pageable);
+}
